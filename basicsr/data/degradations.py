baf53ec0f5638b4ff31f9ae5c7b791193bd0fa89
BoringCrypto <b@rtje.net>
2023-12-22 01:26:03 +1100

    Fix for torchvision 0.17+

--- basicsr/data/degradations.py
+++ basicsr/data/degradations.py
@@ -5,7 +5,13 @@ import random
 import torch
 from scipy import special
 from scipy.stats import multivariate_normal
-from torchvision.transforms.functional_tensor import rgb_to_grayscale
+
+try:
+    # version 0.15 onwards
+    from torchvision.transforms.functional import rgb_to_grayscale
+except:
+    # version < 0.15 - functional_tensor was deprecated in 0.15 and removed in 0.17
+    from torchvision.transforms.functional_tensor import rgb_to_grayscale
 
 # -------------------------------------------------------------------- #
 # --------------------------- blur kernels --------------------------- #
